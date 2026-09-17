1bbc584d3e74ff8463d68580e8d2d6abb932a602
josefinemarelius-cpu <josefine.marelius@gmail.com>
2026-09-17 13:07:18 +0200

    Solved

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ tab30.addEventListener('click', function() {
 
 
 tab60.addEventListener('click', function() {
-  tab30.classList.add('active'); 
-  tab60.classList.remove('active'); 
-  content30.classList.add('active'); 
-  content60.classList.remove('active');
+  tab60.classList.add('active'); 
+  tab30.classList.remove('active'); 
+  content60.classList.add('active'); 
+  content30.classList.remove('active');
 });
 
 // Sökfältsvalidering
@@ -27,12 +27,14 @@ const searchFeedback = document.getElementById('searchFeedback');
 searchInput.addEventListener('input', function() {
   const query = searchInput.value; 
 
-  
-  if (query.length < 3) {
+  if (query.length === 0) {
+    searchFeedback.textContent = "";
+    searchFeedback.className = 'search-feedback';
+  } else if (query.length < 3) {
     searchFeedback.textContent = 'Skriv minst 3 tecken för att söka...';
     searchFeedback.className = 'search-feedback warning';
   } else {
     searchFeedback.textContent = 'Söker efter deltagare: "' + query + '"...';
     searchFeedback.className = 'search-feedback success';
-  }
+  } 
 });
